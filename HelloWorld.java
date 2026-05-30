f97a3417c6fd6dbe20524c86694da11fd4049118
Olif-L <olageleta1@gmail.com>
2026-05-30 19:55:13 -0400

    update HelloWorld.java
    We added age and name in the output

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,10 @@ public class HelloWorld
  public static void main(String[] args)
  {
    System.out.println("Hello world");
+   String name = "NO Name";
+   int age = 25;
+   
+   System.out.println(name);
+   System.out.println(age);
  }
 }
